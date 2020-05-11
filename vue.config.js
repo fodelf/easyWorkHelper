14b05344d7c7bf7e4bc1da713312386d52aeb0ab
wuwenzhou <wuwenzhou@yunzhangfang.com>
2020-05-11 20:23:36 +0800

    第一次提交

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const ispro = process.env.NODE_ENV !== 'development'
 module.exports = {
+  publicPath: ispro ? '' : '/',
+  outputDir: 'easyWorkHelper',
+  assetsDir: 'static',
   lintOnSave: false
 }
