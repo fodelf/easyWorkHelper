c4c8c8765d51556307c4461feeb281fb2698ef1a
吴文周 <wuwenzhou@qq.com.cn>
2020-05-12 08:35:53 +0800

    测试代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
+<!--
+ * @Description: 描述
+ * @Author: 吴文周
+ * @Github: https://github.com/fodelf
+ * @Date: 2020-05-11 21:44:05
+ * @LastEditors: 吴文周
+ * @LastEditTime: 2020-05-11 23:14:47
+ * @FilePath: /workespacemanger/Users/fodelf/git/easyWorkHelper/src/App.vue
+ -->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <van-nav-bar title="小助手" left-text="返回" left-arrow>
+      <template #right>
+        <van-icon name="search" size="18" />
+      </template>
+    </van-nav-bar>
+    <van-collapse v-model="activeNames">
+      <van-collapse-item title="标题1" name="1">内容</van-collapse-item>
+      <van-collapse-item title="标题2" name="2">内容</van-collapse-item>
+      <van-collapse-item title="标题3" name="3">内容</van-collapse-item>
+    </van-collapse>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      activeNames: []
+    }
+  }
+}
+</script>
 
 <style lang="less">
 #app {
@@ -15,18 +44,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  width: 375px;
+  height: 600px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
+/*
+ * @Description: 描述
+ * @Author: 吴文周
+ * @Github: https://github.com/fodelf
+ * @Date: 2020-05-11 21:44:05
+ * @LastEditors: 吴文周
+ * @LastEditTime: 2020-05-11 23:13:49
+ * @FilePath: /workespacemanger/Users/fodelf/git/easyWorkHelper/src/main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/index.css'
+// import 'vant/lib/index.css'
+import 'vant/lib/icon/local.css'
+import Vant from 'vant'
+// import './assets/css/vant.css'
+// import { Collapse, CollapseItem, NavBar} from 'vant'
 
+// Vue.use(Collapse)
+// Vue.use(CollapseItem)
+// Vue.use(NavBar)
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
